7105d8e4ee42de425297da0841f77a1b8f0d2f4b
JRSkates <skatesproduction@gmail.com>
2023-04-13 16:07:29 +0100

    Updated Notes

--- debugging-challenge-3.rb
+++ debugging-challenge-3.rb
@@ -19,6 +19,6 @@ def get_most_common_letter(text)
   # Checked the char and the counter[char] on each loop
   # Saw we were getting the hash correctly - k v pairs of characters and the number of occurrences
   # output was wrong, p out the final return to see the full output
-  # adjusted the final block to give us the key of the highest value
+  # adjusted the final method to give us the key of the highest value
   # adjusted the code within the each loop to ignore spaces as a character as that was passing as the highest value
-  # correct output achieved! 
+  # correct output achieved!
