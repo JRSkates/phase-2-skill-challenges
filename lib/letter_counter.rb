b833f8cd65de59b630c09df245ad92700c8214d5
JRSkates <skatesproduction@gmail.com>
2023-04-17 14:19:29 +0100

    LetterCounter Class Debugged

---
+++ lib/letter_counter.rb
@@ -0,0 +1,27 @@
+# File: letter_counter.rb
+
+class LetterCounter
+  def initialize(text)
+    @text = text
+  end
+  
+  def calculate_most_common()
+    hash = Hash.new(0)
+    @text.downcase.chars.each do |letter| 
+      hash[letter] += 1
+    end
+    hash.max_by{|k,v| v}.reverse!
+  end
+  
+  private
+  
+  def is_letter?(letter)
+    return letter =~ /[a-z]/i
+  end
+end
+  
+counter = LetterCounter.new("Digital Punk")
+p counter.calculate_most_common
+  
+  # Intended output:
+  # [2, "i"]
