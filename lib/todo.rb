f4d0a6e0ab0a8bfa62e2dcd633c4680b5332122f
JRSkates <skatesproduction@gmail.com>
2023-04-17 17:04:52 +0100

    Todo Class Integration Challenge set up

---
+++ lib/todo.rb
@@ -0,0 +1,19 @@
+class Todo
+  def initialize(task)
+    @task = task
+  end
+
+  def task
+    @task
+  end
+
+  def mark_done!
+  # Marks the todo as done
+  # Returns nothing
+  end
+
+  def done?
+  # Returns true if the todo is marked as done
+  # Otherwise returns false
+  end
+end
---
+++ lib/todo_list.rb
@@ -0,0 +1,22 @@
+class TodoList
+  def initialize
+    
+  end
+
+  def add(todo)
+
+  end
+
+  def incomplete
+  # Returns all non-done todos
+  end
+
+  def complete
+  # Returns all complete todos
+  end
+
+  def give_up!
+  # Marks all todos as complete
+  end
+
+end
